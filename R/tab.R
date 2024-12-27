62f09baaa44883639435128ae9d7360cc321477a
George Youssef <99158351+gjyoussef@users.noreply.github.com>
2024-12-27 23:07:47 +1100

    updated docs

--- R/tab.R
+++ R/tab.R
@@ -2,16 +2,15 @@
 #'
 #' Uses approach that is similar to Stata's 'tab1'
 #'
-#' @param df A data frame containing the variables to tabulate.
+#' @param df A dataframe or tibble containing the variables to tabulate.
 #' @param vars A character vector specifying the names of the variables in the data frame.
 #'
 #' @return This function prints the frequency tables for the specified variables.
 #' @export
 #'
 #' @examples
-#' df <- mtcars
-#' tab(df, "mpg") # Use for one variable.
-#' tab(df, c("mpg", "cyl", "disp")) # Use a character vector for multiple.
+#' tab(mtcars, "mpg") # Use for one variable.
+#' tab(mtcars, c("mpg", "cyl", "disp")) # Use a character vector for multiple.
 #'
 tab <- function(df, vars) {
 
