42b4f36aaae4f847e7acf759511dab821f12d77c
George Youssef <99158351+gjyoussef@users.noreply.github.com>
2024-12-27 22:31:03 +1100

    added tests

--- R/tab.R
+++ R/tab.R
@@ -8,11 +8,22 @@
 #'
 #' @examples
 tab <- function(df, vars) {
-  # # Check that vars are in df's column names
-  # missing_vars <- setdiff(vars, colnames(df))
-  # if (length(missing_vars) > 0) {
-  #   warning("The following variables are not found in the data frame: ", paste(missing_vars, collapse = ", "))
-  # }
+
+  # Check that df is a data frame
+  if (!is.data.frame(df)) {
+    stop("The 'df' argument must be a data frame.")
+  }
+
+  # Check that vars is a character vector
+  if (!is.character(vars)) {
+    stop("The 'vars' argument must be a character vector (see docs for examples).")
+  }
+
+  # Check that vars are in df's column names
+  missing_vars <- setdiff(vars, colnames(df))
+  if (length(missing_vars) > 0) {
+    warning("The following variables are not found in the data frame: ", paste(missing_vars, collapse = ", "))
+  }
 
   # Iterate through the specified variables and print frequency tables
   for (i in vars) {
@@ -21,7 +32,3 @@ tab <- function(df, vars) {
     cat("\n\n")
   }
 }
-
-
-#CO2
-#mtcars
