c4f005c28a4cf3c016469426ac14e0610131b671
George Youssef <99158351+gjyoussef@users.noreply.github.com>
2024-12-27 17:44:50 +1100

    added first dependency

---
+++ R/tab.R
@@ -0,0 +1,18 @@
+tab <- function(df, vars) {
+  # # Check that vars are in df's column names
+  # missing_vars <- setdiff(vars, colnames(df))
+  # if (length(missing_vars) > 0) {
+  #   warning("The following variables are not found in the data frame: ", paste(missing_vars, collapse = ", "))
+  # }
+
+  # Iterate through the specified variables and print frequency tables
+  for (i in vars) {
+    print(i)
+    print(tabyl(df[[i]]))  # tabyl comes from janitor
+    cat("\n\n")
+  }
+}
+
+
+#CO2
+#mtcars
